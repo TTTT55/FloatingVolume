7424ff35994aac40fbc241cebc426ec420c71e76
Notorious <35992771+TTTT55@users.noreply.github.com>
2023-10-31 18:55:38 +0530

    Update VolumeKeyService.java

--- app/src/main/java/com/android/mycax/floatingvolume/services/VolumeKeyService.java
+++ app/src/main/java/com/android/mycax/floatingvolume/services/VolumeKeyService.java
@@ -8,25 +8,40 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+import com.android.mycax.floatingvolume.utils.ExpandedVolumeDialog;
+
+import java.util.Objects;
+
 public class VolumeKeyService extends AccessibilityService {
     public VolumeKeyService() {
     }
+    
+    private ExpandedVolumeDialog expandedVolumeDialog;
 
     @Override
     public boolean onKeyEvent(KeyEvent event) {
+
+        expandedVolumeDialog = new ExpandedVolumeDialog(this);
+        final DisplayMetrics metrics = new DisplayMetrics();
+        final WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Objects.requireNonNull(windowManager).getDefaultDisplay().getMetrics(metrics);
+        final LayoutInflater inflater = LayoutInflater.from(this);
+
         int action = event.getAction();
         int keyCode = event.getKeyCode();
         if (action == KeyEvent.ACTION_UP) {
-            if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-                Log.d("Hello", "KeyUp");
-            } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
-                Log.d("Hello", "KeyDown");
+            if (keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+                expandedVolumeDialog.expandView(inflater, metrics);
             }
             return true;
         } else {
             return super.onKeyEvent(event);
         }
     }
+
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
