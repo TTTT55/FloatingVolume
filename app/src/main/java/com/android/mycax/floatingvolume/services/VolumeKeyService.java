7919dfc6376bfd474266507f30a7c595703cca33
Notorious <35992771+TTTT55@users.noreply.github.com>
2023-11-02 21:24:18 +0530

    Update VolumeKeyService.java

--- app/src/main/java/com/android/mycax/floatingvolume/services/VolumeKeyService.java
+++ app/src/main/java/com/android/mycax/floatingvolume/services/VolumeKeyService.java
@@ -12,7 +12,6 @@ import android.content.Context;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.WindowManager;
-import com.android.mycax.floatingvolume.audio.AudioVolumeObserver;
 import com.android.mycax.floatingvolume.utils.ExpandedVolumeDialog;
 
 import java.util.Objects;
@@ -21,56 +20,17 @@ public class VolumeKeyService extends AccessibilityService {
     public VolumeKeyService() {
     }
 
-    private boolean isExpandedVolumeDialogVisible = false;
     private ExpandedVolumeDialog expandedVolumeDialog;
-    private AudioVolumeObserver mAudioVolumeObserver;
-
-    /*@Override
-    public boolean onKeyEvent(KeyEvent event) {
-        int action = event.getAction();
-        int keyCode = event.getKeyCode();
-        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
-
-            expandedVolumeDialog = new ExpandedVolumeDialog(this);
-            final DisplayMetrics metrics = new DisplayMetrics();
-            final WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-
-
-            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                    | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
-                    | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-                    | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                    | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-                    | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
-                    | WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
-
-                
-            Objects.requireNonNull(windowManager).getDefaultDisplay().getMetrics(metrics);
-            final LayoutInflater inflater = LayoutInflater.from(this);
-
-            expandedVolumeDialog.expandView(inflater, metrics);
-            return true;
-        } else {
-            return super.onKeyEvent(event);
-        }
-    }*/
 
     @Override
     public boolean onKeyEvent(KeyEvent event) {
         int action = event.getAction();
         int keyCode = event.getKeyCode();
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_VOLUME_UP:
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
-                if (!isExpandedVolumeDialogVisible) {
-                    // Show the custom volume panel
-                    expandedVolumeDialog = new ExpandedVolumeDialog(this);
-                    final DisplayMetrics metrics = new DisplayMetrics();
-                    final WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-                    WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
+            if (keyCode == KeyEvent.KEYCODE_VOLUME_UP || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+                expandedVolumeDialog = new ExpandedVolumeDialog(this);
+                final DisplayMetrics metrics = new DisplayMetrics();
+                final WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+                WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
                     WindowManager.LayoutParams.WRAP_CONTENT,
                     WindowManager.LayoutParams.WRAP_CONTENT,
                     WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
@@ -80,44 +40,21 @@ public class VolumeKeyService extends AccessibilityService {
                     | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
                     | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
                     | WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
-                    Objects.requireNonNull(windowManager).getDefaultDisplay().getMetrics(metrics);
-                    final LayoutInflater inflater = LayoutInflater.from(this);
-                    expandedVolumeDialog.expandView(inflater, metrics);
-                    isExpandedVolumeDialogVisible = true;
-                } else {
-                    // Update the existing custom volume panel
-                    expandedVolumeDialog = new ExpandedVolumeDialog(this);
-                    final DisplayMetrics metrics = new DisplayMetrics();
-                    final WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-                    WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
-                    WindowManager.LayoutParams.WRAP_CONTENT,
-                    WindowManager.LayoutParams.WRAP_CONTENT,
-                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                    | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN
-                    | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-                    | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                    | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-                    | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
-                    | WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
-                    Objects.requireNonNull(windowManager).getDefaultDisplay().getMetrics(metrics);
-                    final LayoutInflater inflater = LayoutInflater.from(this);
-                    expandedVolumeDialog.expandView(inflater, metrics);
-                }
+                Objects.requireNonNull(windowManager).getDefaultDisplay().getMetrics(metrics);
+                final LayoutInflater inflater = LayoutInflater.from(this);
+                expandedVolumeDialog.expandView(inflater, metrics);
                 return true;
-            default:
+            } else {
                 return super.onKeyEvent(event);
         }
     }
 
-
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-
     }
 
     @Override
     public void onInterrupt() {
-
     }
 
 }
